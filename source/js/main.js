fff1c90d5a6363058e9f0ffa5907a058b316c55a
PoushnayaA <poushnaya@bk.ru>
2024-04-13 00:21:38 +0300

    add header+nav

--- source/js/main.js
+++ source/js/main.js
@@ -6,45 +6,21 @@ import {Form} from './modules/form-validate/form';
 
 window.addEventListener('DOMContentLoaded', () => {
 
-  // Utils
-  // ---------------------------------
+  const navigationButton = document.querySelector('.navigation__wrapper button');
+  const navigationList = document.querySelector('.navigation__list');
 
-  iosVhFix();
+  navigationButton.addEventListener('click', function() {
+    navigationList.classList.toggle('active');
+    navigationButton.classList.toggle('active');
+    if (navigationList.classList.contains('active')) {
+      const navListHeight = navigationList.scrollHeight;
+      navigationList.style.height = `${navListHeight}px`;
+    } else {
+      navigationList.style.height = '0';
+    }
+  });
 
-  // Modules
-  // ---------------------------------
-
-  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
-  // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
-    const form = new Form();
-    window.form = form;
-    form.init();
+    navigationList.style.height = '0';
   });
 });
-
-// ---------------------------------
-
-// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
-
-// привязывайте js не на классы, а на дата атрибуты (data-validate)
-
-// вместо модификаторов .block--active используем утилитарные классы
-// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✅
-
-// выносим все в дата атрибуты
-// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
-
-// для адаптивного JS используется matchMedia и addListener
-// const breakpoint = window.matchMedia(`(min-width:1024px)`);
-// const breakpointChecker = () => {
-//   if (breakpoint.matches) {
-//   } else {
-//   }
-// };
-// breakpoint.addListener(breakpointChecker);
-// breakpointChecker();
-
-// используйте .closest(el)
